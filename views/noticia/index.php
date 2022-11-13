e1212e94d2cd3b9d3f1e53db3154d41167f4ffb9
robe85 <robe.sarria@gmail.com>
2022-11-13 10:59:36 +0100

    Update con imagen

--- views/noticia/index.php
+++ views/noticia/index.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a(Yii::t('app', 'Create Noticia'), ['create'], ['class' => 'btn btn-success']) ?>
+       
     </p>
 
     <?php Pjax::begin(); ?>
