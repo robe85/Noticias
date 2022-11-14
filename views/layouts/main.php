90a146316d3ad819708c061c2c588d106b3ff113
robe85 <robe.sarria@gmail.com>
2022-11-14 12:02:24 +0100

    Update

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,9 +39,9 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => '@w
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Inicio', 'url' => ['/noticia/index']],
+            //['label' => 'About', 'url' => ['/site/about']],
+           // ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest
                 ? ['label' => 'Login', 'url' => ['/site/login']]
                 : '<li class="nav-item">'
